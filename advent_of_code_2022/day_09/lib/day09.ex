072479633a8a8736d0e7c4272fdfcc0e0090ab24
Argonus <argonuspiotr@gmail.com>
2022-12-12 13:33:49 +0100

    a

--- advent_of_code_2022/day_09/lib/day09.ex
+++ advent_of_code_2022/day_09/lib/day09.ex
@@ -2,17 +2,108 @@ defmodule Day09 do
   @moduledoc """
   Documentation for `Day09`.
   """
+  defmodule Accumulator do
+    defstruct rope: [], history: MapSet.new([])
+  end
 
-  @doc """
-  Hello world.
+  @spec part_one(String.t()) :: integer
+  def part_one(file_path) do
+    accumulator = %Accumulator{rope: [{0,0}, {0, 0}]}
 
-  ## Examples
+    file_path
+    |> parse_file()
+    |> Enum.reduce(accumulator, &move_rope/2)
+    |> tail_history()
+  end
 
-      iex> Day09.hello()
-      :world
+  defp tail_history(%{history: tail_history}) do
+    tail_history
+    |> MapSet.to_list()
+    |> length()
+  end
 
-  """
-  def hello do
-    :world
+  @spec part_two(String.t()) :: integer
+  def part_two(file_path) do
+    file_path
+    |> parse_file()
+  end
+
+  ### Rope movements
+  def move_rope({dir, steps}, accumulator) do
+    Enum.reduce(1..steps, accumulator, fn _, %Accumulator{rope: [head_pos, tail_pos], history: history} ->
+      {new_head, new_tail} = dir |> move_head(head_pos) |> move_tail(tail_pos)
+
+      %Accumulator{rope: [new_head, new_tail], history: MapSet.put(history, new_tail)}
+    end)
+  end
+
+  defp move_head("U", {x, y}), do: {x, y + 1}
+  defp move_head("D", {x, y}), do: {x, y - 1}
+  defp move_head("R", {x, y}), do: {x + 1, y}
+  defp move_head("L", {x, y}), do: {x - 1, y}
+
+  defp move_tail(head_pos, {x_tail, y_tail} = tail_pos) do
+    cond do
+      adjacent?(head_pos, tail_pos) ->
+        {head_pos, tail_pos}
+
+      same_y_pos?(head_pos, tail_pos) ->
+        possible_tail_pos = [
+          {x_tail + 1, y_tail},
+          {x_tail - 1, y_tail}
+        ]
+
+        {head_pos, valid_tail_pos(head_pos, possible_tail_pos)}
+
+      same_x_pos?(head_pos, tail_pos) ->
+        possible_tail_pos = [
+          {x_tail, y_tail + 1},
+          {x_tail, y_tail - 1}
+        ]
+
+        {head_pos, valid_tail_pos(head_pos, possible_tail_pos)}
+
+      true ->
+        possible_tail_pos = [
+          {x_tail + 1, y_tail + 1},
+          {x_tail + 1, y_tail - 1},
+          {x_tail - 1, y_tail + 1},
+          {x_tail - 1, y_tail - 1}
+        ]
+
+        {head_pos, valid_tail_pos(head_pos, possible_tail_pos)}
+    end
+  end
+
+  defp adjacent?({x, y}, {x, y}), do: true
+
+  defp adjacent?({x_head, y_head}, {x_tail, y_tail}) do
+    axis_adjacent?(x_head, x_tail) && axis_adjacent?(y_head, y_tail)
+  end
+
+  defp axis_adjacent?(head, tail) do
+    Enum.any?((tail - 1)..(tail + 1), &(&1 == head))
+  end
+
+  defp same_y_pos?({_, y}, {_, y}), do: true
+  defp same_y_pos?(_, _), do: false
+
+  defp same_x_pos?({x, _}, {x, _}), do: true
+  defp same_x_pos?(_, _), do: false
+
+  defp valid_tail_pos(head_pos, possible_tail_pos) do
+    Enum.find(possible_tail_pos, &adjacent?(head_pos, &1))
+  end
+
+  ### Helpers
+
+  defp parse_file(file_path) do
+    file_path
+    |> File.stream!()
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(&String.split/1)
+    |> Stream.map(fn [dir, steps] ->
+      {dir, String.to_integer(steps)}
+    end)
   end
 end
--- advent_of_code_2022/day_09/test/day09_test.exs
+++ advent_of_code_2022/day_09/test/day09_test.exs
@@ -1,8 +1,31 @@
 defmodule Day09Test do
-  use ExUnit.Case
-  doctest Day09
+  use ExUnit.Case, async: true
 
-  test "greets the world" do
-    assert Day09.hello() == :world
+  @example_path_one Path.expand("./fixtures/example.txt", __DIR__)
+  @example_path_two Path.expand("./fixtures/example_two.txt", __DIR__)
+  @input_path Path.expand("./fixtures/input.txt", __DIR__)
+
+  describe "part_one/1" do
+    test "example input" do
+      assert 13 == Day09.part_one(@example_path_one)
+    end
+
+    test "personal input" do
+      assert 6_642 == Day09.part_one(@input_path)
+    end
+  end
+
+  describe "part_two/1" do
+    test "example input one" do
+      assert 1 == Day09.part_two(@example_path_one)
+    end
+
+    test "example input two" do
+      assert 36 == Day09.part_two(@example_path_two)
+    end
+
+    test "personal input" do
+      assert 480_000 == Day09.part_two(@input_path)
+    end
   end
 end
